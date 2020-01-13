37bb05c2df98d3687036e6d209508ff27063b3d1
lennon <qingshanyuluo@gmail.com>
2020-01-13 13:38:40 +0800

    initial

---
+++ src/test/java/xyz/lennon/ecar/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lennon.ecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
